75cf628bf508d19bb1cd6015a0b4c93dde561bfb
Michael Tomae <michael.tomae@gmail.com>
2018-08-21 23:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const main = document.querySelector('#main')
+const paragraph = document.querySelector('p')
 
 main.addEventListener('click', (event) => {
   alert("I was clicked")
+})
+
+paragraph.addEventListener('click', (event)=> {
+  alert("You clicked the p!")
 })
